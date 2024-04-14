9aba5285358b0b6cbf84f1dec0bd119bc73c3bcc
Karmel920 <mikolajpe2@gmail.com>
2024-04-14 23:25:13 +0200

    new test e2e added

--- cypress/e2e/slider.cy.js
+++ cypress/e2e/slider.cy.js
@@ -16,3 +16,27 @@ describe('Swiper Gallery Test', function () {
     cy.get('.swiper-slide-active').should('contain', 'Paris');
   });
 });
+
+describe('Swiper Gallery Test', function () {
+  it('Should swipe slides in gallery with navigation buttons', function () {
+    cy.visit('http://localhost:3000');
+    cy.get('.swiper-button-next').click();
+    cy.wait(2000);
+    cy.get('.swiper-slide-active').should('contain', 'United Kingdom');
+    cy.get('.swiper-button-prev').click();
+    cy.wait(2000);
+    cy.get('.swiper-slide-active').should('contain', 'Rome');
+  })
+});
+
+describe('Swiper Gallery Test', function () {
+  it('Should display proper description', function () {
+    cy.visit('http://localhost:3000');
+    cy.get('[data-test="rome-title"]').should('contain', 'Rome');
+    cy.get('[data-test="italy-description"]').should('contain', 'Italy');
+    cy.get('[data-test="london-title"]').should('contain', 'London');
+    cy.get('[data-test="uk-description"]').should('contain', 'United Kingdom');
+    cy.get('[data-test="paris-title"]').should('contain', 'Paris');
+    cy.get('[data-test="france-description"]').should('contain', 'France');
+  })
+});
